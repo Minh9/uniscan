4d75d1c64dba11f1adfe549e0d2d268a06c32c56
Minh <minh9@pdx.edu>
2021-02-07 13:52:33 -0800

    Update for uniscan

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,77 +1,44 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  GraphToken,
+  Contract,
   Approval,
   MinterAdded,
   MinterRemoved,
   NewOwnership,
   NewPendingOwnership,
   Transfer
-} from "../generated/GraphToken/GraphToken"
-import { ExampleEntity } from "../generated/schema"
+} from "../generated/Contract/Contract"
+import { Account } from "../generated/schema"
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+function loadOrCreateAcccount(address:string):Account {
+  let account =Account.load(address);
+  if(!account==null){
+    account = new Account(address);
+    account.balance = BigInt.fromI32(0)
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  }
+  return account as Account;
+}
+export function handleTransfer(event: Transfer): void {
+  let addresFrom = event.params.from.toHexString()
+  let addressTo = event.params.to.toHexString()
+  let value = event.params.value
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  let isMinting=addresFrom =='0x0000000000000000000000000000000000000'
+  let isBurning =addressTo =='0x00000000000000000000000000000000000'
+
+  if (!isMinting)
+  {
+    let accountFrom = loadOrCreateAcccount(addresFrom)
+    accountFrom.balance= accountFrom.balance.minus(value)
+    accountFrom.save()
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  if(!isBurning)
+  {
+    let accountTo=loadOrCreateAcccount(addressTo)
+    accountTo.balance=accountTo.balance.plus(value)
+    accountTo.save()
+  }
 
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.spender = event.params.spender
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.allowance(...)
-  // - contract.approve(...)
-  // - contract.balanceOf(...)
-  // - contract.decimals(...)
-  // - contract.decreaseAllowance(...)
-  // - contract.governor(...)
-  // - contract.increaseAllowance(...)
-  // - contract.isMinter(...)
-  // - contract.name(...)
-  // - contract.nonces(...)
-  // - contract.pendingGovernor(...)
-  // - contract.symbol(...)
-  // - contract.totalSupply(...)
-  // - contract.transfer(...)
-  // - contract.transferFrom(...)
 }
-
-export function handleMinterAdded(event: MinterAdded): void {}
-
-export function handleMinterRemoved(event: MinterRemoved): void {}
-
-export function handleNewOwnership(event: NewOwnership): void {}
-
-export function handleNewPendingOwnership(event: NewPendingOwnership): void {}
-
-export function handleTransfer(event: Transfer): void {}
